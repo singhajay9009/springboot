b05e7bdcc04d107df9cdb99f620891a95ec7f866
singhajay9009 <singhajay9009@gmail.com>
2022-01-15 20:00:00 +0530

    Add older files

---
+++ fileOperations/src/test/java/com/sample/fileOperations/FileOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.fileOperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ webClient/src/main/java/com/example/webClient/controller/SampleAPIController.java
@@ -0,0 +1,2 @@
+package com.example.webClient.controller;public class SampleAPIController {
+}
