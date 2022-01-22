d67a96bb16ad4532257b2e2ed30810be7c79fa03
singhajay9009 <singhajay9009@gmail.com>
2022-01-22 20:08:20 +0530

    Add AWS Config Property Class

---
+++ s3FileOperations/src/main/java/com/sample/s3Ops/s3FileOperations/config/AwsStorageConfigProperties.java
@@ -0,0 +1,21 @@
+package com.sample.s3Ops.s3FileOperations.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsStorageConfigProperties {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String accessSecret;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+
+}
