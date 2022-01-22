0858776ec2c8e7d27d8a9504d88db4dd13d89310
singhajay9009 <singhajay9009@gmail.com>
2022-01-22 19:58:24 +0530

    Add Spring webflux dependency

---
+++ s3FileOperations/src/main/java/com/sample/s3Ops/s3FileOperations/S3FileOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.sample.s3Ops.s3FileOperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3FileOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3FileOperationsApplication.class, args);
+	}
+
+}
---
+++ s3FileOperations/src/test/java/com/sample/s3Ops/s3FileOperations/S3FileOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.s3Ops.s3FileOperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3FileOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
